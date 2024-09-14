9adc6332f4710cfaca4f5f80a612042c9778e333
Popugairulat <150455922+Popugairulat@users.noreply.github.com>
2024-09-14 19:14:33 +0300

    Add files via upload

---
+++ 1_week/CMakeProject_1_week_task-1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject_1_week_task-1.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject_1_week_task-1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
