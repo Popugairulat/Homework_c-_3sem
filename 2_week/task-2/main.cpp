99c84811fa8c2bfd3f9c680e0b09f0ecc474b205
Popugairulat <150455922+Popugairulat@users.noreply.github.com>
2024-10-12 17:41:56 +0300

    Add files via upload

---
+++ 2_week/task-2/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "utils.h"
+
+#ifndef WIDTH
+#define WIDTH 480
+#endif
+
+#ifndef HEIGHT
+#define HEIGHT 320
+#endif
+
+int main() {
+    std::string path("my_image.ppm");
+    write_image(path, WIDTH, HEIGHT);
+    return 0;
+}
---
+++ 2_week/task-2/utils.h
@@ -0,0 +1,8 @@
+﻿#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+void write_image(const std::string& path, int width, int height);
+
+#endif // UTILS_H
